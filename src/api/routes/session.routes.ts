b0001585321e59f6f875aec13c2954b5b6422873
Juan Rafael Saavedra Sanguinetti <juan.rafael.saavedra@fing.edu.uy>
2021-05-16 17:53:27 -0300

    Added input validations with express-validator.

--- src/api/routes/session.routes.ts
+++ src/api/routes/session.routes.ts
@@ -1,6 +1,7 @@
 import { SessionService } from '../../services';
 import { Router, Response, Request } from 'express';
 import middlewares from '../middlewares';
+import { body, validationResult, param } from 'express-validator';
 
 const route = Router()
 
@@ -9,9 +10,23 @@ export const SessionRouter = (router: Router, service: SessionService): void => 
     router.use('/session', route);
 
     // Login
-    route.post('/', async (req: Request, res: Response) => {
+    route.post('/', 
+
+        // Validations //      
+        // email is not empty
+        body('email').notEmpty(),
+        // password is not empty
+        body('password').notEmpty(), 
+
+        async (req: Request, res: Response) => {
         try {
-            console.log("Login " + req.body);
+            // Check validation errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) { 
+              return res.status(400).json({ errors: errors.array() });
+            }
+
+            // Call service
             const { email, password } = req.body;
             console.log(email + password);
             const data = await service.SignIn(email, password);
--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { UserService } from '../../services';
 import { Router, Response, Request } from 'express';
 import middlewares from '../middlewares';
+import { body, validationResult } from 'express-validator';
 
 const route = Router()
 
@@ -8,21 +9,28 @@ export const UserRouter = (router: Router, service: UserService): void => {
     
     router.use('/user', route);
 
-    // TODO delete this method Get user by email
-    route.get('/:email', async (req: Request, res: Response) => {
-        try {
-            const { email } = req.params;
-            const data = await service.GetUser(email);
-            res.status(200).send(data);
-        }
-        catch (err) {
-            res.status(500).send({ "err": err })
-        }
-    })
+    // Create user - Sign up
+    route.post('/', 
 
-    // Create user
-    route.post('/', async (req: Request, res: Response) => {
+      // Validations //      
+      // email must be an email
+      body('email').isEmail(),
+      // password must be at least 5 chars long 
+      body('password').isLength({ min: 5 }),
+      // firstname not empty
+      body('firstname').notEmpty(),
+      // lastname not empty
+      body('lastname').notEmpty(),
+
+      async (req: Request, res: Response) => {
         try {
+            // Check validation errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) { 
+              return res.status(400).json({ errors: errors.array() });
+            }
+
+            // Call service
             const newUser = req.body;
             const data = await service.CreateUser(newUser);
             res.status(200).send(data);
@@ -33,28 +41,41 @@ export const UserRouter = (router: Router, service: UserService): void => {
     })
 
     // Add favourite movie
-    route.post('/:userId/movie', middlewares.isAuth, async (req: Request, res: Response) => {
-        try {
-            const { userId } = req.params;
-            const { movieId } = req.body;
-            const data = await service.AddFavouriteMovie(+userId, movieId);
-            res.status(200).send(data);
-        }
-        catch (err) {
-            res.status(500).send({ "err": err })
-        }
-    })
- 
-    // Get user favourite movies
-    route.get('/:id/movie', middlewares.isAuth, async (req: Request, res: Response) => {
-        try {
-            console.log("Get favourite movies")
-            const userId = req.params.id;
-            const data = await service.GetFavouriteMovies(+userId);
-            res.status(200).send(data);
-        }
-        catch (err) {
-            res.status(500).send({ "err": err })
-        }
-    })
+    route.post('/:userId/movie', middlewares.isAuth,
+
+        // Validations //
+        // movieId not empty
+        body('movieId').notEmpty(),
+
+        async (req: Request, res: Response) => {
+            try {
+                // Check validation errors
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) { 
+                  return res.status(400).json({ errors: errors.array() });
+                }
+
+                // Call service
+                const { userId } = req.params;
+                const { movieId } = req.body;
+                const data = await service.AddFavouriteMovie(+userId, movieId);
+                res.status(200).send(data);
+            }
+            catch (err) {
+                res.status(500).send({ "err": err })
+            }
+        })
+     
+        // Get user favourite movies
+        route.get('/:id/movie', middlewares.isAuth, async (req: Request, res: Response) => {
+            try {
+                console.log("Get favourite movies")
+                const userId = req.params.id;
+                const data = await service.GetFavouriteMovies(+userId);
+                res.status(200).send(data);
+            }
+            catch (err) {
+                res.status(500).send({ "err": err })
+            }
+        })
 }
